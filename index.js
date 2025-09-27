83788ac81fb3a47e83909eec2d819caeaabf075e
Marc Lefebvre <markaether@macbookpro.home>
2025-09-27 14:08:19 +0200

    Remove Next.js plugin and Next.js dependency

--- index.js
+++ index.js
@@ -1,4 +1,11 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+
 console.log('Friendly Circle App fonctionne !');
-export default function Home() {
+
+function Home() {
   return <h1>Bienvenue sur Friendly Circle App</h1>;
 }
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<Home />);
